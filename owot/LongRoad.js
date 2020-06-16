1dcdbde57ef85e6aa76d35121dd2e2d6d53ab564
Ruichka <42318266+Ruichka@users.noreply.github.com>
2020-06-16 17:24:41 +0300

    Update LongRoad.js

--- owot/LongRoad.js
+++ owot/LongRoad.js
@@ -1,10 +1,10 @@
-var speed = 1
-writeCharTo(" ", "#000000", 0, 0, 0, 3)
+var speed = 1;
+writeCharTo(" ", "#000000", 0, 0, 0, 3);
 
-var bruh = setInterval(a, 50)
+var bruh = setInterval(a, 50);
 
 function a() {
   for (var i=0;i<speed;i++) {
-    moveCursor("right")
-  }
-}
+    moveCursor("right");
+  };
+};
