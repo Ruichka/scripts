5bf8a3cad414e70c18f08569f8b6848a07af5434
Ruichka <42318266+Ruichka@users.noreply.github.com>
2020-08-02 06:55:54 +0300

    Update notscript.js

--- owot/notscript.js
+++ owot/notscript.js
@@ -1,12 +1,10 @@
-class NotScript() {
-  function printText(text,tileX,tileY,charX,charY) {
-    for (let i=0;i<text.length;i++) {
-      charX++;
-      if (charX == 16) {
-        tileX++
-        charX = 0
-      }
-      writeCharTo("text[i]",tileX,tileY,charX,charY);
-    };
+function printText(text,tileX,tileY,charX,charY) {
+  for (let i=0;i<text.length;i++) {
+    charX++;
+    if (charX == 16) {
+      tileX++
+      charX = 0
+    }
+    writeCharTo("text[i]",tileX,tileY,charX,charY);
   };
 };
