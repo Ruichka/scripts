bc9c30513209e5bdf5b9388cbc2e7e303547c489
Ruichka <42318266+Ruichka@users.noreply.github.com>
2020-08-02 06:41:44 +0300

    Create notscript.js

---
+++ owot/notscript.js
@@ -0,0 +1,12 @@
+class NotScript() {
+  function print(text,tileX,tileY,charX,charY) {
+    for (let i=0;i<text.length;i++) {
+      charX++;
+      if (charX == 16) {
+        tileX++
+        charX = 0
+      }
+      writeCharTo("text[i]",tileX,tileY,charX,charY);
+    };
+  };
+};
