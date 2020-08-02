5488b19f1d6be539690481ddfe947e9ca447f10a
Ruichka <42318266+Ruichka@users.noreply.github.com>
2020-08-02 07:07:08 +0300

    Update notscript.js

--- owot/notscript.js
+++ owot/notscript.js
@@ -1,10 +1,11 @@
 function printText(text,tileX,tileY,charX,charY) {
+  charX--
   for (let i=0;i<text.length;i++) {
     charX++;
     if (charX == 16) {
       tileX++
       charX = 0
     }
-    writeCharTo("text[i]",tileX,tileY,charX,charY);
+    writeCharTo("text[i]",0,tileX,tileY,charX,charY);
   };
 };
