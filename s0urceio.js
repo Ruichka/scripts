955ff6b020d1dec5516af8ec267566efc8c5672d
Ruichka <42318266+Ruichka@users.noreply.github.com>
2020-08-16 03:01:06 +0300

    Create s0urceio.js

---
+++ s0urceio.js
@@ -0,0 +1,9 @@
+var e = ["root","bit","host","remove","url","intel","user","upload","stat","send","system","anon","temp","loop","pass","http","global","call","status","port","socket","domain","ping","dir","right","buffer","data","delete","reset","file","num","add","com","info","left","point","get","count","xml","ghost","type","handle","log","init","event","join","list","add","val","signal","net","set","write","cookies","part","load","bytes","emit","size","client","key","poly"]
+var m = []
+var h = []
+
+addEventListener("keydown",function(e){
+	if (e.key == "KeyC") {
+		var f = 
+	}
+})
